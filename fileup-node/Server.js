b7faacbb63ab70154b027a70a5c0aba16071ba22
kenu <kenu.heo@gmail.com>
2015-06-01 13:46:02 +0900

    reformat

--- fileup-node/Server.js
+++ fileup-node/Server.js
@@ -1,6 +1,6 @@
 // code from https://github.com/codeforgeek/File-upload-in-Node/
 // Tutorial link : http://wp.me/p4ISPV-cq
-var express    =       require("express");
+var express    =       require('express');
 var multer     =       require('multer');
 var app        =       express();
 var done       =       false;
@@ -19,13 +19,13 @@ onFileUploadComplete: function (file) {
 }));
 
 app.get('/',function(req,res){
-      res.sendfile("index.html");
+      res.sendfile('index.html');
 });
 
 app.post('/api/photo',function(req,res){
   if(done==true){
     console.log(req.files);
-    res.end("File uploaded.");
+    res.end("File uploaded.\n" + JSON.stringify(req.files));
   }
 });
 
