6ed29f66f0a7db9d595ae252c1ff339b15ff811a
kenu <kenu.heo@gmail.com>
2015-06-01 16:15:57 +0900

    code tidy

--- fileup-node/Server.js
+++ fileup-node/Server.js
@@ -1,34 +1,35 @@
 // code from https://github.com/codeforgeek/File-upload-in-Node/
 // Tutorial link : http://wp.me/p4ISPV-cq
-var express    =       require('express');
-var multer     =       require('multer');
-var app        =       express();
-var done       =       false;
+var express = require('express');
+var multer = require('multer');
+var app = express();
+var done = false;
 
-app.use(multer({ dest: './uploads/',
- rename: function (fieldname, filename) {
-    return filename+Date.now();
-  },
-onFileUploadStart: function (file) {
-  console.log(file.originalname + ' is starting ...')
-},
-onFileUploadComplete: function (file) {
-  console.log(file.fieldname + ' uploaded to  ' + file.path)
-  done=true;
-}
+app.use(multer({
+    dest: './uploads/',
+    rename: function (fieldname, filename) {
+        return Date.now();
+    },
+    onFileUploadStart: function (file) {
+        console.log(file.originalname + ' is starting ...')
+    },
+    onFileUploadComplete: function (file) {
+        console.log(file.fieldname + ' uploaded to  ' + file.path)
+        done = true;
+    }
 }));
 
-app.get('/',function(req,res){
-      res.sendfile('index.html');
+app.get('/', function (req, res) {
+    res.sendfile('index.html');
 });
 
-app.post('/api/photo',function(req,res){
-  if(done==true){
-    console.log(req.files);
-    res.end("File uploaded.\n" + JSON.stringify(req.files));
-  }
+app.post('/api/photo', function (req, res) {
+    if (done == true) {
+        console.log(req.files);
+        res.end("File uploaded.\n" + JSON.stringify(req.files));
+    }
 });
 
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log("Working on port 3000");
 });
