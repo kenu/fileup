6c09acba4df0b9c378ab77a7156efb998a90db85
kenu <kenu.heo@gmail.com>
2022-08-09 15:39:47 +0900

    rm jquery

--- fileup-node/Server.js
+++ fileup-node/Server.js
@@ -3,18 +3,17 @@ const express = require("express");
 const formidable = require("formidable");
 
 const app = express();
+app.use(express.static("uploads"));
 
 app.get("/", (_req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/api/photo", (req, res, next) => {
-  console.log(req.files);
   const form = formidable({
     uploadDir: __dirname + "/uploads/",
     filename: Date.now(),
   });
-
   form.parse(req, (err, fields, files) => {
     if (err) {
       next(err);
